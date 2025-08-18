6b35f55b4acc30392622d2068f2cb205e5cd40d3
Cnnb01 <nyamambanyaguch@gmail.com>
2025-08-18 18:14:32 +0300

    Set up [slug].astro to render blog details dynamically

--- landing-panda/src/data/types.ts
+++ landing-panda/src/data/types.ts
@@ -1,20 +1,29 @@
 interface BlogPost {
   id: number;
   date: string;
+  slug: string;
   title: {
     rendered: string;
   };
   guid: {
     rendered: string;
-  }
+  };
   content: {
     rendered: string;
   };
   excerpt: {
     rendered: string;
   };
-  link:string
-  // add other fields you care about
+  link: string;
+  author: number;
+  jetpack_featured_media_url: string;
+
+  // embedded data
+  _embedded?: {
+    author?: {
+      name: string;
+    };
+  };
 }
 
 export type { BlogPost };
